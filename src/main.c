d456670af37292afd9928356859a97be9be377d7
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2014-08-22 13:53:02 +0200

    Documenting main.c

--- src/main.c
+++ src/main.c
@@ -15,13 +15,43 @@ for small satellites. The star sensor is the best option in terms of accuracy an
 performance. However, the major drawbacks of developing these systems are cost, 
 weight and the effort required in the production process. This is why GranaSAT team 
 is going to design and build a <b> low-cost attitude determination system</b>. The team will 
-test the system, which is going to be based in a star sensor, a horizon sensor and the 
-magnetic field measurements provided by a magnetometer. 
+test the system, which is going to be based in a <b>star sensor</b>, a <b>horizon sensor</b> and the 
+<b>magnetic field measurements</b> provided by a magnetometer. 
 The same Charge Coupled Device is going to be used for both the star sensor and the 
 horizon sensor to obtain the orientation of the gondola. Furthermore, we will estimate 
 the attitude off-board by using the magnetic field measurements obtained during the 
 flight.
 
+ * @section mission_sec Mission statement
+ * The GranaSAT experiment aims to study the orientation determination in satellites. 
+A device called <b> star tracker </b> is normally used to obtain the information about the 
+orientation of a satellite in 3-dimensional coordinates. However, if the camera fails to 
+detect the stellar field, we will use the measurements provided by the <b> magnetometer 
+sensor </b>, the <b> accelerometer sensor </b> and a <b> horizon sensor </b> to calculate the attitude. 
+The experiment is designed to be placed in a university GranaSAT pico satellite. 
+Hence, performing the experiment in conditions environmentally similar to those in 
+outer space may be very instructive for us. 
+
+ * @section objectives_sec Experiment objectives
+ * @subsec Primary objectives
+ * - The main objective of this experiment is to build and test in real conditions an 
+attitude measurement system during the BEXUS 19 campaign.
+ * - To test the accuracy of the attitude measurement system using a star sensor.
+ * - To test the accuracy of the attitude measurement system using a horizon 
+sensor.
+ * - To test the accuracy of the attitude measurement system using a 
+magntometer.
+ * - To check whether or not the attitude system is adequate for the GranaSAT pico 
+satellite. 
+
+ * @subsec Secondary objectives
+ * - To gain know-how about space missions and their future application in other 
+UGR space projects.
+ * - To learn how to document a space related project.
+ * - To improve cooperative work skills.
+
+ * @section readDoc_sec How to read this documentation?
+ * @todo THIS SECTION HAS TO BE FULFILLED.
  */
 
 // C standard libraries
