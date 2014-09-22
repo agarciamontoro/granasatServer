5733677765c19520cf74db6f88383814dae3994e
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2014-09-22 19:26:08 +0200

    Minor changes in the script

--- bin/granasatServer.sh
+++ bin/granasatServer.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Trap code that will be executed if the main program is interrupted.
+int_trap() {
+    echo
+    echo "[SCRIPT]: GranaSAT Server interrupted."
+    sudo killall -15 granasatServer
+}
+
+#Sets the trap
+trap int_trap INT
+
 #Creation of the new folder name, in order not to override the previous measurements
 LAST_FOLDER=`ls /home/pi/GranaSAT/ | grep OUTPUT | tail -n1`
 LAST_NUMBER=`echo $LAST_FOLDER | cut -d_ -f2`
@@ -16,4 +26,4 @@ mkdir $OUTPUT_FOLDER/LSM303
 mkdir $OUTPUT_FOLDER/TEMPs
 
 #Start server processs
-sudo /home/pi/GranaSAT/BIN/granasatServer $OUTPUT_FOLDER 2> $OUTPUT_FOLDER/LOG/$OUTPUT_LOG
+sudo /home/pi/GranaSAT/BIN/granasatServer $OUTPUT_FOLDER 2>&1 >/dev/null | sudo tee $OUTPUT_FOLDER/LOG/$OUTPUT_LOG
